7562f24ec7cc03dc439fcecb0e5d2c71e06e1395
chinnygan <chinnygan@hotmail.co.uk>
2016-03-12 16:59:03 +0000

    testing in informatics

--- IR_Transmitter/IR_Transmitter.runs/synth_1/runme.sh
+++ IR_Transmitter/IR_Transmitter.runs/synth_1/runme.sh
@@ -36,4 +36,4 @@ EAStep()
      fi
 }
 
-EAStep vivado -log Processor_Wrapper.vds -m64 -mode batch -messageDb vivado.pb -notrace -source Processor_Wrapper.tcl
+EAStep vivado -log IRTransmitterMaster.vds -m64 -mode batch -messageDb vivado.pb -notrace -source IRTransmitterMaster.tcl
